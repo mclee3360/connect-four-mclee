d3d587816e96049b24b9a610e9d71819bdbe1a31
Michael Lee <mclee3360@gmail.com>
2021-04-29 15:56:38 -0500

    Create initial state hook for square

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 export default function Square() {
+  const [checked, setChecked] = useState(false);
+  const handleClickEvent = () => checked || setChecked(true);
+
   return (
-    <button className="Square"></button>
+    <button className="Square" onClick={handleClickEvent}>
+      <div className="Square-content">{checked ? 'X' : ''}</div>
+    </button>
   );
 }
