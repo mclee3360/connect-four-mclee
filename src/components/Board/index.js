082dc27e6aea66d8cd75404ed83eaed9d78b8e89
Michael Lee <mclee3360@gmail.com>
2021-04-30 15:39:40 -0500

    Check for win condition

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -22,14 +22,19 @@ export default function Board() {
     newRowState[col] = p1Turn ? 1 : 2;
     newBoardState[droppedRow] = newRowState;
 
+    setBoardState(newBoardState);
+    setP1Turn(!p1Turn);
+
+    if (checkForWin(newBoardState)) {
+      setDisabledDroppers(disabledDroppers.map((dropper) => true));
+      return;
+    };
+
     if (droppedRow === 0) {
       const newDisabledDroppers = disabledDroppers.slice();
       newDisabledDroppers[col] = true;
       setDisabledDroppers(newDisabledDroppers);
     }
-
-    setBoardState(newBoardState);
-    setP1Turn(!p1Turn);
   };
 
   const renderDroppers = (num_cols) => disabledDroppers.map((dropper, col) => (
@@ -60,4 +65,78 @@ const getRowToDrop = (board, col) => {
     }
   }
   return -1;
+};
+
+const checkForWin = (board) => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (
+        (j < 4 && checkForHorizontalWin(board, i, j)) ||
+        ((i < 3) && (
+          checkForVerticalWin(board, i, j) ||
+          (j < 4 && checkForDiagRightWin(board, i, j)) ||
+          (j > 2 && checkForDiagLeftWin(board, i, j))
+        ))
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const checkForHorizontalWin = (board, startRow, startCol) => {
+  if (startCol > board[startRow].length - 4) {
+    return false;
+  }
+  if (!board[startRow][startCol]) {
+    return false;
+  }
+  return (
+    board[startRow][startCol] === board[startRow][startCol + 1] &&
+    board[startRow][startCol] === board[startRow][startCol + 2] &&
+    board[startRow][startCol] === board[startRow][startCol + 3]
+  );
+};
+
+const checkForVerticalWin = (board, startRow, startCol) => {
+  if (startRow > board.length - 4) {
+    return false;
+  }
+  if (!board[startRow][startCol]) {
+    return false;
+  }
+  return (
+    board[startRow][startCol] === board[startRow + 1][startCol] &&
+    board[startRow][startCol] === board[startRow + 2][startCol] &&
+    board[startRow][startCol] === board[startRow + 3][startCol]
+  );
+};
+
+const checkForDiagRightWin = (board, startRow, startCol) => {
+  if (startCol > board[startRow].length - 4 || startRow > board.length - 4) {
+    return false;
+  }
+  if (!board[startRow][startCol]) {
+    return false;
+  }
+  return (
+    board[startRow][startCol] === board[startRow + 1][startCol + 1] &&
+    board[startRow][startCol] === board[startRow + 2][startCol + 2] &&
+    board[startRow][startCol] === board[startRow + 3][startCol + 3]
+  );
+};
+
+const checkForDiagLeftWin = (board, startRow, startCol) => {
+  if (startCol < board[startRow].length - 4 || startRow > board.length - 4) {
+    return false;
+  }
+  if (!board[startRow][startCol]) {
+    return false;
+  }
+  return (
+    board[startRow][startCol] === board[startRow + 1][startCol - 1] &&
+    board[startRow][startCol] === board[startRow + 2][startCol - 2] &&
+    board[startRow][startCol] === board[startRow + 3][startCol - 3]
+  );
 };
