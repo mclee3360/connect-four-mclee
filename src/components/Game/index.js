8c8e96243c48d9ac0112ac73aeea9c752a8e4aab
Michael Lee <mclee3360@gmail.com>
2021-05-17 14:20:30 -0500

    Add game reset function

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -5,6 +5,7 @@ import './index.css';
 export default function Game() {
   const [player, setPlayer] = useState(1);
   const [winner, setWinner] = useState(0);
+  const [gameCount, setGameCount] = useState(0);
 
   const renderPlayer = (player, winner) => {
     if (winner) {
@@ -13,14 +14,21 @@ export default function Game() {
     return <p>It’s Player {player}’s Turn</p>;
   }
 
+  const resetGame = () => {
+    setPlayer(1);
+    setWinner(0);
+    setGameCount(gameCount + 1);
+  };
+
   return (
     <article className="Game">
       <section className="Game-play">
         <h1>Connect 4</h1>
-        <Board player={player} updatePlayer={setPlayer} updateWinner={setWinner} />
+        <Board key={gameCount} player={player} updatePlayer={setPlayer} updateWinner={setWinner} />
       </section>
       <section>
         {renderPlayer(player, winner)}
+        <button onClick={resetGame}>Reset Game</button>
       </section>
     </article>
   );
